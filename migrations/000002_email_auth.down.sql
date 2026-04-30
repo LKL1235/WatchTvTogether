e82e1a92d86137ef616fcec0b3eea03b2c696cd4
Huang Nan <2544563843@qq.com>
2026-04-30 17:53:29 +0800

    remove unnecessary sql

--- migrations/000002_email_auth.down.sql
+++
@@ -1,2 +0,0 @@
-DROP INDEX IF EXISTS idx_users_email_lower;
-ALTER TABLE users DROP COLUMN IF EXISTS email;
--- migrations/000003_drop_download_tasks.up.sql
+++
@@ -1,2 +0,0 @@
-DROP INDEX IF EXISTS idx_download_tasks_created_at;
-DROP TABLE IF EXISTS download_tasks;
